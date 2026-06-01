324b39531aa2fd40efeee8906df7432625899635
f-jooste <fourie.j789@gmail.com>
2026-06-01 13:10:29 +0200

    ADD: Read and write function

--- echo_server.js
+++ echo_server.js
@@ -2,9 +2,18 @@ import * as net from "net";
 
 function newConn(socket) {
     console.log('new connection', socket.remoteAddress, socket.remotePort);
+
+    socket.on('end', () => {// FIN received. The connection will be closed automatically.
+        console.log("EOF.");
+    });
+
+    socket.on('data', (data) => {
+    console.log('data', data); // Log the received data
+    socket.write(data); // Send the received data back to the client
+});
 }
 
 let server = net.createServer();
-server.on('connection', newConn);
 server.on('error', (err) => {throw err; });
+server.on('connection', newConn);
 server.listen({host: '127.0.0.1', port: 1234});
